153a9dfd2c7628fd566627a99a5a51e2fbfcb3f8
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-05-22 14:04:07 -0700

    finally get orders to list, update

--- PierresVendors/Controllers/VendorsControllers.cs
+++ PierresVendors/Controllers/VendorsControllers.cs
@@ -44,7 +44,7 @@ namespace PierresVendors.Controllers
       Order newOrder = new Order(orderTitle, orderDate, orderDescription, orderPrice);
       thisVendor.AddOrder(newOrder);
       List<Order> vendorOrders = thisVendor.Orders;
-      model.Add("order", vendorOrders);
+      model.Add("orders", vendorOrders);
       model.Add("vendor", thisVendor);
       return View("Show", model);
     }
--- PierresVendors/Models/Vendors.cs
+++ PierresVendors/Models/Vendors.cs
@@ -8,7 +8,7 @@ namespace PierresVendors.Models
     public string Name { get; set; }
     public string Description { get; set; }
 
-    public List<Order> Orders { get; set; }
+    public List<Order> Orders = new List<Order> { };
     public int Id { get; set; }
     public static List<Vendor> VendorList = new List<Vendor> { };
 
